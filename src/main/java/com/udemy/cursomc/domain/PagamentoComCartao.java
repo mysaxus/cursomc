1678dfae07c004a74a094d15cbf49480582f42d7
mysaxus <927misael@gmail.com>
2020-09-25 14:57:01 -0300

    pedido estadopagamento e pagamento

---
+++ src/main/java/com/udemy/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,33 @@
+package com.udemy.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.udemy.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento{
+	private static final long serialVersionUID = 1L;
+
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+		
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento pagamento, Pedido pedido, 
+			Integer numeroDeParcelas) {
+		super(id, pagamento, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+	
+}
