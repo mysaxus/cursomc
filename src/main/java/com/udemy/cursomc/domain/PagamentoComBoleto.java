fd6768e73f55f81094229924c70cb3c049f7bd61
mysaxus <927misael@gmail.com>
2020-09-25 15:32:09 -0300

    endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/udemy/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/udemy/cursomc/domain/PagamentoComBoleto.java
@@ -4,13 +4,17 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.udemy.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
-
+	
+	@JsonFormat(pattern="dd/M/yyyy")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern="dd/M/yyyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
